bf73aa5647968d3e0eedb170b9cc6af6268a95e7
Dezan <you@example.com>
2024-07-04 19:48:15 +0700

    tugas

--- js/script.js
+++ js/script.js
@@ -1,1 +1,21 @@
-// ini Java Script
+// username interactive
+function validateForm() {
+    const name = document.forms["message-form"]["name-input"].value
+    if (name == ""){
+        document.getAnimations("errorrname").innerHTML = "Tidak Boleh Kosong"
+
+        return false 
+    }
+    document.getElementById("name").innerHTML = name;
+        document.getElementById("errorrname").innerHTML = "";
+    setName (name);
+    return false;
+}
+
+
+function setName (name) {
+    document.getElementById("name").innerHTML = name;
+    document.getElementById("errorrname").innerHTML = ""
+
+}
+
